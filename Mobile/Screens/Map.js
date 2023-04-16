8bc185c1888fb422ebbfa2ce160171fbe4afb503
Khryx <khryxbergolafermat@gmail.com>
2023-04-17 00:28:29 +0800

    testing

---
+++ Mobile/Screens/Map.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TextInput, StyleSheet, Text, Linking, Button, Alert, View, Image, } from 'react-native';
+
+function Map(props) {
+    return (
+        <Text>Hello</Text>
+    );
+}
+
+export default Map;
